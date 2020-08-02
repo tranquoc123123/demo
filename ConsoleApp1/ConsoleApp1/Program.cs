492f0b40babbeef8f72737e63e39553ab3f0353c
tranquoc123123 <49123953+tranquoc123123@users.noreply.github.com>
2020-08-02 16:02:40 +0700

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("xyzxhjakhsdj");
             Console.WriteLine("xyz");
         }
     }
