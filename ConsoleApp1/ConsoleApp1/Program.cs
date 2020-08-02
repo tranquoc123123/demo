5d976e520c06ac79b3efa05c3defc3911994b880
tkquoc <tranquoc123123@gmail.com>
2020-08-02 16:18:51 +0700

    change source

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("xyzxhjakhsdj qư  ewq");
-            Console.WriteLine("xyz");
+            Console.WriteLine("xyz ạdkuyiwiqoe");
         }
     }
 }
