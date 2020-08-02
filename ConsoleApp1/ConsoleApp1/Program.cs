3e9024e93128175c72c0d49e0de865e6b338b6bb
tkquoc <tranquoc123123@gmail.com>
2020-08-02 16:25:29 +0700

    áda

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("xyzxhjakhsdj qư  ewq111111111111111111111");
-            Console.WriteLine("xyz ạdkuyiwiqoe");
+            Console.WriteLine(" zzzz xyz ạdkuyiwiqoe");
         }
     }
 }
